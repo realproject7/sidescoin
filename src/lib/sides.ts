98371f81fef1577405d9133c87ad504a160b5138
Cho Young-Hwi <cyh76507707@gmail.com>
2026-08-26 19:30:37 +0900

    fix: keep launch narrative off the site

--- src/lib/sides.ts
+++ src/lib/sides.ts
@@ -3,6 +3,3 @@ export const SIDES_CONTRACT =
 
 export const SIDES_MARKET_URL =
   `https://lptoken.fun/trade/base/${SIDES_CONTRACT}`;
-
-export const SIDES_LAUNCH_TRANSACTION_URL =
-  "https://basescan.org/tx/0xac77d38e3c71fefa9d5955aa3154a76da3b4810e0a3025d13fc59fff511deb06";
